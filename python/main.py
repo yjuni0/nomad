aea3a5e4f27842221ed78c9425b7b02444f0f988
OYEs <160278000+yjuni0@users.noreply.github.com>
2024-07-24 11:38:05 +0900

    requests-get

--- python/main.py
+++ python/main.py
@@ -67,19 +67,26 @@ while playing:
 """days_of_week = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"]
 print(days_of_week[0])"""
 
-"""
-websites = [
+from requests import get
+
+websites = (
   "https://google.com",
   "airbnb.com",
   "https://twitter.com",
   "facebook.com",
   "tiktok.com",
   "instagram.com"
-]
+)
 
+results = {}
 for website in websites: 
   if not website.startswith("https://"):
     website = f"https://{website}"
-  print(website)
-"""
+  response = get(website)
+  if response.status_code == 200:
+    results[website] = "OK"
+  else:
+    results[website] = "Failed"
+print(results)
+
 
