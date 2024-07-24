1161ce64b55c3e8545e9967cdee9df7514bf199e
OYEs <160278000+yjuni0@users.noreply.github.com>
2024-07-24 12:28:21 +0900

    Create class.py

---
+++ python/class.py
@@ -0,0 +1,10 @@
+class Puppy:
+
+  def __init__(self):
+    self.name = "Ruffus"
+    self.age = 2
+    self.breed = "beagle"
+
+ruffus = Puppy()
+bibi = Puppy()
+print(bibi.name,bibi.age,bibi.breed)
