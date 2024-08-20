15162e45c43ac14ac90b13350f832aede6e04f22
OYEs <160278000+yjuni0@users.noreply.github.com>
2024-08-20 19:57:45 +0900

    webscrapper

--- python/practice/webscrapper/file.py
+++ python/practice/webscrapper/file.py
@@ -2,21 +2,24 @@ from flask import Flask, render_template
 
 app = Flask("JobScrapper")
 
+
 @app.route("/")
 def home():
-  return render_template("home.html", name="nico")
+    return render_template("home.html", name="nico")
+
 
 @app.route("/hello")
 def hello():
-  return "hello you"
+    return "hello you"
+
 
 @app.route("/search")
 def search():
-  keyword = request.args.get("keyword")
-  indeed = extract_indeed_jobs(keyword)
-  wwr = extract_wwr_jobs(keyword)
-  jobs = indeed + wwr
-  return render_template("search.html", keyword=keyword, jobs=jobs)
+    keyword = request.args.get("keyword")
+    indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    jobs = indeed + wwr
+    return render_template("search.html", keyword=keyword, jobs=jobs)
 
 
 app.run("0.0.0.0")
--- python/practice/webscrapper/scraping.py
+++ python/practice/webscrapper/scraping.py
@@ -7,7 +7,6 @@ def job_scraping(url):
     response = requests.get(url)
 
     soup = BeautifulSoup(response.content, "html.parser",)
-
     jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
 
     for job in jobs:
@@ -41,7 +40,7 @@ def job_scraping(url):
 def get_pages(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser",)
-    return len(soup.find("div", class_="pagination").find_all("span", class_="page"))
+    return len(soup.find("div", class_=".pagination").find_all("span", class_=".page"))
 
 total_pages = get_pages("https://weworkremotely.com/remote-full-time-jobs?page=1")
 
